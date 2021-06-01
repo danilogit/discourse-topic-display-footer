bfe0bba7958791aeea7dc5ffe68d9f75beddd09e
Danilo L <hello@danilo.cc>
2021-06-01 14:54:09 -0300

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# discourse-topic-display-footer
+A theme component that add an HTML code below each topic.
---
+++ javascripts/discourse/initializers/initialize-for-topic-display-footer.js
@@ -0,0 +1,30 @@
+import { withPluginApi } from "discourse/lib/plugin-api";
+import Mobile from "discourse/lib/mobile";
+
+
+export default {
+  name: "topic-display-footer",
+  initialize() {
+    withPluginApi("0.8.41", api => {
+      try {
+        api.decorateCookedElement(
+          (post, helper) => {
+
+            console.log(post, helper, settings);
+            const topic_footer_html =  settings.footer_html;
+            const cooked = $(post);
+            cooked.append(topic_footer_html)
+
+          },
+          {
+            id: "topic-display-footer",
+            onlyStream: true
+          }
+        );
+      } catch (error) {
+        
+        console.error(error);
+      }
+    });
+  }
+};
