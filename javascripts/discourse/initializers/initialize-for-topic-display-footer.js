e43c36a8579bdebe663d22cac61e6ee7785752d6
Danilo Lima <hello@danilo.cc>
2021-06-23 15:28:01 -0300

    Update initialize-for-topic-display-footer.js

--- javascripts/discourse/initializers/initialize-for-topic-display-footer.js
+++ javascripts/discourse/initializers/initialize-for-topic-display-footer.js
@@ -24,7 +24,7 @@ export default {
           },
           {
             id: "topic-display-footer",
-            onlyStream: true
+            onlyStream: false
           }
         );
       } catch (error) {
